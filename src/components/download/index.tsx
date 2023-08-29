eb7e08b9861ab30e2464bc8c42c62760412c24f5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-29 15:31:22 +0900

    Update index.tsx

--- src/components/download/index.tsx
+++ src/components/download/index.tsx
@@ -8,7 +8,7 @@ export default ((props: Props) => {
   const [password, setPassword] = createSignal(props.hash().split('-').slice(1).join(''))
   
   return <div>
-    <div>
+    <div class="mx-5">
       <div class='text-2xl'>Download</div>
       <div>
         <div>
@@ -16,6 +16,18 @@ export default ((props: Props) => {
           <input value={password()} onInput={(evt) => {
             setPassword(evt.target.value)
           }}/>
+        </div>
+        <div>
+          <button class='outlined-button' onClick={async () => {
+            const url = `https://api.end2end.tech/download?id=${id}`
+            const blobUrl = URL.createObjectURL(await fetch(url).then(res => res.blob()))
+            const aTag = document.createElement('a')
+            aTag.download = 'unknown'
+            aTag.href = blobUrl
+            aTag.click(aTag)
+          }}>
+            Download!
+          </button>
         </div>
       </div>
     </div>
